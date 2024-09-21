cbc96354e87d1c4bfff1598edfdd85fd488d0da1
Sanhil143 <sanhilrai143@gmail.com>
2024-09-21 16:53:23 +0530

    NEW:Auth-login/signup
    created login/signup mechanism

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
-import Footer from './components/common/Footer'
+import {BrowserRouter as Router,Route,Routes,Link} from 'react-router-dom'
 import Landingpage from './components/Landingpage'
+import Auth from './components/layout/Auth'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 function App() {
   return(
-    <>
-      <Landingpage/>
-    </>
+    <Router>
+      <Routes>
+      <Route path='/' element={<Landingpage/>}/>
+      <Route path='/login' element={<Auth/>}/>
+      </Routes>
+    </Router>
   )
 }
 
